be4a95f9af6ed7bf4e7db1266af9cb87623bf225
felips_dulfz <felipinanerdulfo@gmail.com>
2026-01-09 20:09:08 +0800

    Making HTTP calls in Angular Services

---
+++ src/app/services/todos.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Todo } from '../model/todo.type';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodosService {
+  todoItems: Array<Todo> = [
+    {
+      title: 'Learn Angular',
+      completed: false,
+      userId: 1,
+      id: 1
+    },
+    {
+      title: 'Build an Angular App',
+      completed: false,
+      userId: 1,
+      id: 2
+    },
+    {
+      title: 'Deploy the App',
+      completed: false,
+      userId: 1,
+      id: 3
+    },    
+  ];
+}
